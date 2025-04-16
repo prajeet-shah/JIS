3f22d7b57fa92b9c060a5a4d9101b87a02119a1b
prajeet shah <prajeetshah93@gmail.com>
2025-04-16 22:19:13 +0545

    resolved cors issue

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ const authRouter = require("./routes/authRoute");
 const profileRouter = require("./routes/profileRoute");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
